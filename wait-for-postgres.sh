a671c1a861b3be2849cdde99c994bbc165ed2f48
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-08-16 18:54:40 +0700

    fix deploy..

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -2,7 +2,7 @@
 # Chờ Postgres sẵn sàng trước khi chạy app
 host="$1"
 shift
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   echo "Postgres is unavailable - sleeping"
   sleep 2
 done
