126c3d0504f8a982e85bc35e782a75997c4da2dc
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-08-25 10:46:26 +0700

    fix deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -134,7 +134,8 @@ CACHES = {
 if ENVIRONMENT == 'production':
     CACHES['default'] = {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        # 'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'memcached:11211',
     }
 
 # CACHES = {
