4d8f6587d343a90c10825aa87b97213e305cffe7
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-10-23 19:18:31 +0700

    fix url

--- apps/trading/urls.py
+++ apps/trading/urls.py
@@ -8,8 +8,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('vps', TradingViews.as_view(), name='trading-views'),
-    path('is_trading', TradingViewsIsTrading.as_view(), name='trading-views'),
+    path('vps/', TradingViews.as_view(), name='trading-views'),
+    path('is_trading/', TradingViewsIsTrading.as_view(), name='trading-views'),
     path('scheduler/start/', StartSchedulerAPIView.as_view(), name='start_scheduler'),
     path('scheduler/stop/', StopSchedulerAPIView.as_view(), name='stop_scheduler'),
     path('scheduler/status/', SchedulerStatusAPIView.as_view(), name='scheduler_status'),
