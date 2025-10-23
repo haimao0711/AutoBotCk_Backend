ce861f6509d676140883f05bfa45c0093480aada
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-10-23 19:24:39 +0700

    fix ALLOWED_HOSTS

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,7 +3,7 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0,*").split(",")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "*"]
 
 # Database: đồng bộ với POSTGRES_* env
 DATABASES['default'].update({
