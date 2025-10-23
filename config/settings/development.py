0e39e546b8e474902d57eb41890476a9b0f36c67
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-10-23 19:09:31 +0700

    fix cele 2

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,7 +3,7 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0,*").split(",")
 
 # Database: đồng bộ với POSTGRES_* env
 DATABASES['default'].update({
