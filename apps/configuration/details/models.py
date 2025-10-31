5ae176786ff570b4867cf99382c529e7eaa21bbc
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-10-31 14:34:15 +0700

    add stoch_rsi_to_take_profit 2

--- apps/configuration/details/models.py
+++ apps/configuration/details/models.py
@@ -352,16 +352,13 @@ class Configuration(models.Model):
     # stoch_rsi
 
     stock_config_use_stoch_rsi_to_buy = models.BooleanField(default=True)
-    stock_config_use_stoch_rsi_obl_to_buy = models.BooleanField(default=True)
-    stock_config_use_stoch_rsi_to_take_profit = models.BooleanField(default=True)
+    stock_config_use_stoch_rsi_obl_to_buy = models.BooleanField(default=True)    
     stock_config_use_stoch_rsi_reversed_increase = models.BooleanField(
         default=True)
     stock_config_value_stoch_rsi_to_buy = models.FloatField(
         default=30, validators=[MinValueValidator(0), MaxValueValidator(100)])
     stock_config_value_stoch_rsi_obl_to_buy = models.FloatField(
-        default=80, validators=[MinValueValidator(0), MaxValueValidator(100)])
-    stock_config_value_stoch_rsi_to_take_profit = models.FloatField(
-        default=80, validators=[MinValueValidator(0), MaxValueValidator(100)])
+        default=80, validators=[MinValueValidator(0), MaxValueValidator(100)])    
     stock_config_stoch_rsi_to_buy_necessary_condition = models.BooleanField(
         default=False)
     stock_config_stoch_rsi_to_buy_sufficient_condition = models.BooleanField(
@@ -609,6 +606,11 @@ class Configuration(models.Model):
         default=True, null=True)
     stock_config_use_bolinger_a_part_to_take_profit = models.BooleanField(
         default=True, null=True)
+    stock_config_use_stoch_rsi_to_take_profit = models.BooleanField(default=True)
+    stock_config_value_stoch_rsi_to_take_profit = models.FloatField(
+        default=80, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    stock_config_percent_stoch_rsi_to_take_profit = models.FloatField(
+        default=0.5, validators=[MinValueValidator(0), MaxValueValidator(1)], null=True)
     stock_config_percent_bolinger_a_part_to_take_profit = models.FloatField(
         default=0.5, validators=[MinValueValidator(0), MaxValueValidator(1)], null=True)
     
