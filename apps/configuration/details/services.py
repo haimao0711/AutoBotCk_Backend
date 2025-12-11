37c172109d026ff47989afcacd51805b85860693
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-12-11 08:53:33 +0700

    fix log add candle second

--- apps/configuration/details/services.py
+++ apps/configuration/details/services.py
@@ -143,7 +143,7 @@ class ConfigurationServices:
 
     @staticmethod
     def update_config_type_configuration(user, update_data):
-        print('đã chạy hàm update_config_type_configuration')
+        logger.info('đã chạy hàm update_config_type_configuration')
         vnindex_config = update_data.get('vnindex_config')
         stock_config = update_data.get('stock_config')
         template, check, config_template = None, True, None
@@ -170,22 +170,21 @@ class ConfigurationServices:
             return ErrorType.UPDATE_FAILED, {}
 
         candle = CandleService.get_candle(candle_type=candle_type)
-        # candle_second = CandleService.get_candle_second(candle_second_type=candle_second_type)
+        candle_second = CandleService.get_candle_second(candle_second_type=candle_second_type)
         candle_sell = CandleService.get_candle_sell(candle_sell_type=candle_sell_type)
-        # candle_sell_second = CandleService.get_candle_sell_second(candle_sell_second_type=candle_sell_second_type)
+        candle_sell_second = CandleService.get_candle_sell_second(candle_sell_second_type=candle_sell_second_type)
         # init update data
         update_config_type_data = {
             'config_type': template.id,
             'user': user.id,
             'stock': stock_id,
             'candle': candle.id,
-            'candle_second': candle_type,
+            'candle_second': candle_second.id,
             'candle_sell': candle_sell.id,
-            'candle_sell_second': candle_sell_second_type,
+            'candle_sell_second': candle_sell_second.id,
             'account': None,
         }
-        logger.info(f'check candle_second: {candle_type}')
-        logger.info(f'check candle_sell_second: {candle_sell_second_type}')
+        logger.info(f'check update_config_type_data: {update_config_type_data}')
         # handle input case
         if 'is_buy' in update_data and config_template.is_buy != update_data['is_buy']:
             update_config_type_data['is_buy'] = update_data['is_buy']
@@ -282,7 +281,9 @@ class ConfigurationServices:
             return ErrorType.UPDATE_FAILED, {'error': 'No matching configurations found for user.'}
 
         candle = CandleService.get_candle(candle_type=candle_type)
+        candle_second = CandleService.get_candle_second(candle_second_type=candle_second_type)
         candle_sell = CandleService.get_candle_sell(candle_sell_type=candle_sell_type)
+        candle_sell_second = CandleService.get_candle_sell_second(candle_sell_second_type=candle_sell_second_type)
         result_list = []
 
         for config_template in all_configs:
@@ -291,20 +292,11 @@ class ConfigurationServices:
                 'user': user.id,
                 'stock': config_template.stock.id,
                 'candle': candle.id,
+                'candle_second': candle_second.id,	
                 'candle_sell': candle_sell.id,
+                'candle_sell_second': candle_sell_second.id,
                 'account': None,
-            }
-            
-            # Chỉ cập nhật candle_second nếu có giá trị trong update_data
-            if candle_second_type:
-                candle_second = CandleService.get_candle_second(candle_second_type=candle_second_type)
-                update_config_type_data['candle_second'] = candle_second.id
-            
-            # Chỉ cập nhật candle_sell_second nếu có giá trị trong update_data
-            if candle_sell_second_type:
-                candle_sell_second = CandleService.get_candle_sell_second(candle_sell_second_type=candle_sell_second_type)
-                update_config_type_data['candle_sell_second'] = candle_sell_second.id
-
+            }        
             # Chỉ cập nhật nếu khác giá trị hiện tại
             if 'is_buy' in update_data and config_template.is_buy != update_data['is_buy']:
                 update_config_type_data['is_buy'] = update_data['is_buy']
