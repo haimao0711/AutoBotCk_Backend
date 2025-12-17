0f14748942c9f5688d4e7aa0ce03f30987eeea4a
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-12-17 10:17:50 +0700

    fix enum OFF

--- apps/configuration/candle/services.py
+++ apps/configuration/candle/services.py
@@ -28,14 +28,30 @@ class CandleService:
     
     @staticmethod
     def get_candle_second(candle_second_type):
+        # Chuẩn hóa giá trị 'Off' hoặc 'None' thành 'OFF' (key trong enum)
+        if candle_second_type in ['Off', 'None', 'NONE']:
+            candle_second_type = 'OFF'
         try:
             candle_second = Candle.objects.get(candle=candle_second_type)
             return candle_second
         except (Candle.DoesNotExist):
+            # Nếu không tìm thấy, tạo mới Candle object với 'OFF'
+            if candle_second_type == 'OFF':
+                candle_second = Candle.objects.create(
+                    name='OFF',
+                    candle='OFF',
+                    candle_second='OFF',
+                    candle_sell='OFF',
+                    candle_sell_second='OFF'
+                )
+                return candle_second
             raise ValueError(ErrorMessages.CANDLE_DOES_NOT_EXIST)
     
     @staticmethod
     def get_candle_sell_second(candle_sell_second_type):
+        # Chuẩn hóa giá trị 'Off' hoặc 'None' thành 'OFF' (key trong enum)
+        if candle_sell_second_type in ['Off', 'None', 'NONE']:
+            candle_sell_second_type = 'OFF'
         # Tìm Candle object có candle_sell=candle_sell_second_type (giống như get_candle_sell)
         # vì các Candle objects được tạo với trường candle_sell, không phải candle_sell_second
         try:
@@ -47,5 +63,15 @@ class CandleService:
                 candle_sell_second = Candle.objects.get(candle=candle_sell_second_type)
                 return candle_sell_second
             except (Candle.DoesNotExist):
+                # Nếu không tìm thấy, tạo mới Candle object với 'OFF'
+                if candle_sell_second_type == 'OFF':
+                    candle_sell_second = Candle.objects.create(
+                        name='OFF',
+                        candle='OFF',
+                        candle_second='OFF',
+                        candle_sell='OFF',
+                        candle_sell_second='OFF'
+                    )
+                    return candle_sell_second
                 raise ValueError(ErrorMessages.CANDLE_DOES_NOT_EXIST)
             
