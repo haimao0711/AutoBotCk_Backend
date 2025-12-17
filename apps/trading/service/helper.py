142cc4d1da247bd44780a4d81f6746ec87b4a0bb
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-12-17 11:29:21 +0700

    fix candle None Type

--- apps/trading/service/helper.py
+++ apps/trading/service/helper.py
@@ -704,10 +704,11 @@ def  should_buy(
             }
         obj_retured = {}
         is_use_candle_following_second = following_config.is_use_candle_second
-        chart_following_second = following_config.candle_second.candle
+        # Kiểm tra None trước khi truy cập .candle để tránh lỗi 'NoneType' object has no attribute 'candle'
+        chart_following_second = getattr(getattr(following_config, "candle_second", None), "candle", "OFF")
         logger.info(f'check chart_following_second: {chart_following_second}')
         is_use_candle_trading_second = trading_config.is_use_candle_second
-        chart_trading_second = trading_config.candle_second.candle  
+        chart_trading_second = getattr(getattr(trading_config, "candle_second", None), "candle", "OFF")
         logger.info(f'check chart_trading_second: {chart_trading_second}')      
         # Tính toán kết quả cho following và following_second
         following, following_reasons = should_buy_chart(
