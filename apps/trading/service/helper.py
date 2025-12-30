4db24dc98591d1b80bc5ebb679de02cd11aa2dc4
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-12-30 11:13:36 +0700

    format render mesage 10

--- apps/trading/service/helper.py
+++ apps/trading/service/helper.py
@@ -141,16 +141,20 @@ def send_telegram_message_batch(user, message_type: MessageTypeEnum, batch_messa
 def render_type(chart_type: str, chart_value: str) -> str:
     match chart_type:
         case ChartType.Trading:
-            return f'''  \n    **▶ Chart hành động** **{chart_value}:**
+            return f''' 
+                **▶ Chart hành động** **{chart_value}:**
 '''
         case ChartType.TradingSecond:
-            return f'''  \n    **▶ Chart hành động (phụ)** **{chart_value}:**
+            return f''' 
+                **▶ Chart hành động (phụ)** **{chart_value}:**
 '''
         case ChartType.Following:
-            return f'''  \n    **▶ Chart theo dõi** **{chart_value}:**
+            return f''' 
+                **▶ Chart theo dõi** **{chart_value}:**
 '''
         case ChartType.FollowingSecond:
-            return f'''  \n    **▶ Chart theo dõi (phụ)** **{chart_value}:**
+            return f''' 
+                **▶ Chart theo dõi (phụ)** **{chart_value}:**
 '''
         case _:
             return f'''
