6844cdbacacd2c68ca7225b9c9f785acee8eb510
meobientnk34 <tranhaimaotnk34@gmail.com>
2026-01-23 09:42:22 +0700

    add log PC1 BVH

--- apps/trading/service/handlers.py
+++ apps/trading/service/handlers.py
@@ -1445,7 +1445,7 @@ def process_trading(prepared: dict, user: User, vnindex_stock: any, vps_account:
             if is_buy_following and not is_buy:
                 send_telegram_message(user, MessageTypeEnum.OVERALL, status_signal=status_buy, **buy_attrs)
             
-            if symbol in ['PC1', 'BID']:
+            if symbol in ['PC1', 'BVH']:
                 send_telegram_message(user, MessageTypeEnum.OVERALL, status_signal=status_buy, **buy_attrs)
             is_send_order_buy = False   
             if status_buy == SignalTelegramEnum.BUY_SUCCESS:
