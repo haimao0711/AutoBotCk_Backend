15390d4280b5160f113c1ef7385d13a77189e550
meobientnk34 <tranhaimaotnk34@gmail.com>
2026-05-07 09:22:41 +0700

    add message MWG, FTS

--- apps/trading/service/handlers.py
+++ apps/trading/service/handlers.py
@@ -1820,8 +1820,8 @@ def process_trading(prepared: dict, user: User, vnindex_stock: any, vps_account:
             if is_buy_following and not is_buy:
                 send_telegram_message(user, MessageTypeEnum.OVERALL, status_signal=status_buy, **buy_attrs)
             
-            # if symbol in ['PC1', 'BVH']:
-            #     send_telegram_message(user, MessageTypeEnum.OVERALL, status_signal=status_buy, **buy_attrs)
+            if symbol in ['MWG', 'FTS']:
+                send_telegram_message(user, MessageTypeEnum.OVERALL, status_signal=status_buy, **buy_attrs)
             is_send_order_buy = False   
             if status_buy == SignalTelegramEnum.BUY_SUCCESS:
                 logger.info(f'bắt đầu hàm đặt lệnh buy {symbol} (Đã có lock từ đầu)')                
